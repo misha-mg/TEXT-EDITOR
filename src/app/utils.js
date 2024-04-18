e010ac3805698c89fe56cf0b0afe0bb410e0868f
Mike <mishahorod@gmail.com>
2024-04-18 17:06:34 +0200

    mergeAdjacentElements with texts

--- src/app/utils.js
+++ src/app/utils.js
@@ -74,7 +74,9 @@ export function removingEmptyTegs(ref) {
 }
 
 export function compareObjects(obj1, obj2) {
-  if (!obj1 || !obj2) {
+  if (!obj1 && !obj2) {
+    return true;
+  } else if (!obj1 || !obj2) {
     return false;
   }
 
